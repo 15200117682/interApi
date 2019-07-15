89ce6c135ca1be090f41302aa77dd392378478ec
yuanshuai <yuanshuai@localhost.localdomain>
2019-07-15 06:29:22 +0000

    bug 注册接口

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -16,47 +16,46 @@ class UserController extends Controller
     //注册
     public function reg(Request $request){
         $regData = $request->input();
-        if($regData['username'] == "" || $regData['pwd'] == "" || $regData['pwd_confirm'] == "" || $regData['email'] == ""){
-            $error = [
+        if(empty($regData['username']) || empty($regData['pwd']) || empty($regData['pwd_confirm']) || empty($regData['email'])){
+            $arr = [
                 'code' => 40000,
-
                 'msg' => '内容为空',
                 'data' => [],
             ];
-            echo json_encode($error,JSON_UNESCAPED_UNICODE);die;
-        }else{
-            if($regData['pwd'] == $regData['pwd_confirm']){
-                $pwd=password_hash($regData['pwd'],PASSWORD_DEFAULT);
-                $arr=[
-                    "username"=>$regData["username"],
-                    "pwd"=>$pwd,
-                    "email"=>$regData["email"],
-                    "time"=>time()
+            return json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }//非空验证
+        if(!$regData['pwd'] == $regData['pwd_confirm']){
+            $arr = [
+                'code' => 40001,
+                'msg' => '请检查确认密码和密码是否一致',
+                'data' => [],
+            ];
+            return json_encode($arr,JSON_UNESCAPED_UNICODE);
+        }//密码错误
+        $pwd=password_hash($regData['pwd'],PASSWORD_DEFAULT);
+        $arr=[
+            "username"=>$regData["username"],
+            "pwd"=>$pwd,
+            "email"=>$regData["email"],
+            "time"=>time()
+        ];
+        $userReg = UserModel::insertGetId($arr);
+            if($userReg){
+                $arr= [
+                    'code' => 200,
+                    'msg' => 'success 请等待跳转',
+                    'data' => $userReg,
                 ];
-                $userReg = UserModel::insertGetId($regData);
-                if($userReg){
-                    return [
-                        'code' => 200,
-                        'msg' => 'success 请等待跳转',
-                        'data' => $userReg,
-                    ];
-                }else{
-                    $error = [
-                        'code' => 40002,
-                        'msg' => '注册失败请重新尝试',
-                        'data' => [],
-                    ];
-                    echo json_encode($error,JSON_UNESCAPED_UNICODE);die;
-                }
+                return json_encode($arr,JSON_UNESCAPED_UNICODE);
             }else{
-                $error = [
-                    'code' => 40001,
-                    'msg' => '请检查确认密码和密码是否一致',
+                $arr = [
+                    'code' => 40002,
+                    'msg' => '注册失败请重新尝试',
                     'data' => [],
                 ];
-                echo json_encode($error,JSON_UNESCAPED_UNICODE);die;
+                return json_encode($arr,JSON_UNESCAPED_UNICODE);
             }
         }
 
-    }
+
 }
