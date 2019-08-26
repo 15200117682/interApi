14b079dbe4112e9a3993f11c568974a23344faa0
yuanshuai <yuanshuai@localhost.localdomain>
2019-08-26 07:07:18 +0000

    分类的model

---
+++ app/Model/CoryModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CoryModel extends Model
+{
+    protected $table="cartcory";
+    protected $primaryKey="c_id";
+    public $timestamps=false;
+}
